1b65a2e14b7bf71d00e73a88bab3b531db1bb62f
Tarik <tarik.jaidani@gmail.com>
2026-03-24 20:45:09 +0100

    push

--- 28.cpp
+++ 28.cpp
@@ -2,7 +2,33 @@
 #include <vector>
 using namespace std;
 
-
 int main() {
+
+
+	vector<int> Number = { 0,1,2,7,8 };
+
+	cout << Number[4] << endl;
+
+	cout <<  Number.size() << endl;
+	
+	Number.push_back(9) ;
+
+
+	cout << Number[5] << endl;
+
+	cout << Number.size();
+	cout << endl;
+
+	for (int i = 0; i < Number.size(); i++) {
+		cout << Number[i];
+	}
+	cout << endl;
+
+
+
+
+	for (int i : Number) {
+		cout << i;
+	}
 
 }
