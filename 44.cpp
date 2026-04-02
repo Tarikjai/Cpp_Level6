fd4ece9c8d45ba1536e5e13cab766913708dc5ae
Tarik <tarik.jaidani@gmail.com>
2026-04-03 01:11:58 +0200

    push

---
+++ 44.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+
+
+int main() {
+
+
+	int num;
+	cout << "Please enter the number of Student: ";
+	cin >> num;
+
+	float* ptr;
+
+
+	ptr = new float[num];
+
+
+	for (int i = 0; i <= num, i++){
+		cout << "Student :"
+	}
+	 
+
+
+
+
+}
