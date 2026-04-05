cbb598ce2c1be0927ab184904e0d4f4e44769d7e
Tarik <tarik.jaidani@gmail.com>
2026-04-05 15:01:39 +0200

    push

---
+++ 47.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+
+	vector <int> vVector = { 1,5,6,9,7 };
+
+	cout << "Initial Vectro: " ;
+
+	for (const int& i : vVector) {
+		cout << i << " "; 
+	}
+	cout << endl;
+
+	cout << "\nUpdated Vectr1: ";
+
+	for ( int& i : vVector) {
+		i = 5;
+		cout << i << " ";
+	}
+	cout << endl;
+
+	cout << "\nUpdated Vectr2: ";
+
+	vVector[0] = 89;
+	vVector.at(1) = 33;
+	vVector.at(4) = 22;
+
+	for (const int& i : vVector) {
+		
+		cout << i << " ";
+	}
+	cout << endl;
+
+}
