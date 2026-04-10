35aa0586d2af221633b4553aee46fdc75a88bb11
Tarik <tarik.jaidani@gmail.com>
2026-04-10 23:24:33 +0200

    push

--- 47.cpp
+++ 47.cpp
@@ -7,7 +7,7 @@ int main() {
 
 	vector <int> vVector = { 1,5,6,9,7 };
 
-	cout << "Initial Vectro: " ;
+	cout << "Initial Vector: " ;
 
 	for (const int& i : vVector) {
 		cout << i << " "; 
---
+++ 57.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+
+void LoadDataFromVectorToFile(string FileName, vector <string> vFileContent) {
+ 
+
+
+
+}
+
+
+
+int main() {
+
+ 
+
+}
