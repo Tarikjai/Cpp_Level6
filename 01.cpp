55d9a26a3e0e0aa0070f3ec4b14b55c19f0b32ac
Tarik <tarik.jaidani@gmail.com>
2026-04-18 17:49:40 +0200

    push

--- 01.cpp
+++ 01.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
+#include <iomanip>
+#include "MyLib.h"
 
 using namespace std;
 
 
+
+
+
 int main() {
 
-	cout << "Hello";
+	int arr[3][3],
+
+		GenerateArray();
+
+
+
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout <<   setw(5) <<MyLib::RandomNumber(1, 101) ;
+		}
+
+		cout << endl;
+	} 
+
+
+		
 }
