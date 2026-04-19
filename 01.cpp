0dd85cbff0edfd12047a829df2a99a89a4acd03b
Tarik <tarik.jaidani@gmail.com>
2026-04-19 15:27:27 +0200

    push

--- 01.cpp
+++ 01.cpp
@@ -5,24 +5,39 @@
 using namespace std;
 
 
+void GenerateRandomNumber(int arr[3][3], int Rows, int Column) {
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Column; j++) {
+			arr[i][j] = MyLib::RandomNumber(1, 100);
+		}
+
+	}
+
+}
 
 
-
-int main() {
-
-	int arr[3][3],
-
-		GenerateArray();
-
-
+void PrintRandomNumber(int arr[3][3]) {
 
 	for (int i = 0; i < 3; i++) {
 		for (int j = 0; j < 3; j++) {
-			cout <<   setw(5) <<MyLib::RandomNumber(1, 101) ;
+			cout << arr[i][j] << " ";
 		}
-
 		cout << endl;
-	} 
+
+	}
+
+}
+
+int main() {
+
+	int arr[3][3];
+
+	
+	GenerateRandomNumber(arr,3,3);
+	PrintRandomNumber(arr);
+	
+	//
 
 
 		
