30049a46cdc8dd454ff5a995e2fc5f01d3c08a79
Tarik <tarik.jaidani@gmail.com>
2026-04-22 23:22:29 +0200

    push

--- Source.cpp
+++ Source.cpp
@@ -8,20 +8,48 @@ void FillArrayWithRadom3X3(int arr[3][3], int Rows, int Cols) {
 
 	for (int i = 0; i < Rows; i++) {
 		for (int j = 0; j < Rows; j++) {
-			arr[i][j] = MyLib::RandomNumber(1, 100);
+			arr[i][j] = MyLib::RandomNumber(1, 9);
 		}
 	}
 }
  
-void printRadom3X3(int arr[3][3], int Rows, int Cols) {
+void printRadom3X3(int arr[3][3], int Rows, int j) {
 
 	cout << "\nThe following is a 3x3 random matrix:\n";
+
 	for (int i = 0; i < Rows; i++) {
 		for (int j = 0; j < Rows; j++) {
 			cout << setw(3) << arr[i][j] << "   ";
 		}
 		cout << endl;
 	}
+}
+
+
+
+int SumCols(int arr[3][3], int Rows, int j) {
+	
+	int sum = 0;
+
+		for (int i = 0; i < Rows; i++) {
+
+			sum += arr[i][j];
+		
+	}
+	return sum;
+
+
+}
+
+
+void PrintSumCols(int arr[3][3], int Rows, int Cols) {
+
+	cout << "\nThe following are the sum of each col in the matrix: \n";
+	for (int j = 0; j<= Cols-1;j++) {
+
+		cout << "Col " << j + 1 << " Sum = " << SumCols(arr, Rows, j) << endl;
+	}
+	
 }
 
 
@@ -34,5 +62,7 @@ int main() {
 	FillArrayWithRadom3X3(arr, 3, 3);
 
 	printRadom3X3(arr, 3, 3);
+
+	PrintSumCols(arr, 3, 3);
 
 }
