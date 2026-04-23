ad5eaff182f8913589b9b2b33cfce1de2c88de93
Tarik <tarik.jaidani@gmail.com>
2026-04-24 00:33:39 +0200

    push

--- 04.cpp
+++ 04.cpp
@@ -3,7 +3,7 @@
 #include "MyLib.h"
 
 using namespace std;
- 
+
 void GenerateMatrix3X3(int arr[3][3], int Rows, int Cols) {
 	for (int i = 0; i < Rows; i++) {
 		for (int j = 0; j < Cols; j++) {
@@ -12,11 +12,12 @@ void GenerateMatrix3X3(int arr[3][3], int Rows, int Cols) {
 	}
 }
 void PrintMatrix3X3(int arr[3][3], int Rows, int Cols) {
+
 	cout << "The following is a 3x3 random matrix: \n";
 
 	for (int i = 0; i < Rows; i++) {
 		for (int j = 0; j < Cols; j++) {
-			cout << setw(3)<< arr[i][j] << "   ";
+			cout << setw(3) << arr[i][j] << "   ";
 		}
 		cout << endl;
 	}
@@ -26,43 +27,38 @@ void PrintMatrix3X3(int arr[3][3], int Rows, int Cols) {
 int SumCols(int arr[3][3], int Rows, int Cols) {
 
 	int Sum = 0;
+
 	for (int i = 0; i < Rows; i++) {
-			Sum += arr[i][Cols];
+
+		Sum += arr[i][Cols];
+
 	}
 	return Sum;
 }
 
 
-void InputColsSum(int arr[3][3], int Rows, int Cols, int NewArr[3]) {
 
-	 
+void PrintColsSum(int arr[3][3], int Rows, int Cols) {
+
+	cout << "\nThe following are the sum of each col in the matrix: \n";
+
 	for (int i = 0; i <= Cols - 1; i++) {
-		 
-		NewArr[i] = SumCols(arr, Rows, i )  ;
+		cout << " Col " << i + 1 << " Sum = " << SumCols(arr, Rows, i) << endl;
 	}
+
+
 	cout << endl;
 }
 
-void PrintColsSum(int NewArr[3], int Rows, int Cols) {
-
-	 cout << "\nThe following are the sum of each col in the matrix: \n";
-
-	for (int i = 0; i <= Cols - 1; i++) {
-
-		cout << " Col " << i + 1 << " Sum = " << NewArr[i]  << endl; 	}
-	cout << endl;
-}
 
 
 int main() {
-	 
-	int arr[3][3];
-	int NewArr[3];
 
-	GenerateMatrix3X3(arr, 3,3);
+	int arr[3][3];
+
+	GenerateMatrix3X3(arr, 3, 3);
 	PrintMatrix3X3(arr, 3, 3);
 
-	InputColsSum(arr, 3, 3, NewArr);
-	
-	PrintColsSum(NewArr, 3, 3);
+	PrintColsSum(arr, 3, 3);
+
 }
