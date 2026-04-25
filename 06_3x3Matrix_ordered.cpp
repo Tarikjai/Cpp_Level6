1ee364cf05d95ddd3d0b112a6df5d10882197459
Tarik <tarik.jaidani@gmail.com>
2026-04-25 22:36:40 +0200

    push

--- 06_3x3Matrix_ordered.cpp
+++ 06_3x3Matrix_ordered.cpp
@@ -6,15 +6,33 @@ using namespace std;
 
 void Generate3X3Matrix(int arr[3][3], int Rows, int Cols) {
 
+	int sum = 1;
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Rows; j++) {
 
+			arr[i][j] += sum; 
+		}
+		
+	}
 }
 
- 
+void PrintMatrix(int arr[3][3], int Rows, int Cols) {
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Rows; j++) {
+
+			cout << setw(3) << arr[i][j] << " ";
+		}
+		cout << endl;
+	}
+}
 
 int main() {
  
 	int arr[3][3];
 
 	Generate3X3Matrix(arr, 3, 3);
+
+	PrintMatrix(arr, 3, 3);
 
 }
