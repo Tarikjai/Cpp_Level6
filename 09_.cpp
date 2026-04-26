39d4bd2114fe0e428a5f19068cdb0f2fef7e1e36
Tarik <tarik.jaidani@gmail.com>
2026-04-27 01:56:38 +0200

    push

---
+++ 09_.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iomanip>
+#include "MyLib.h"
+
+using namespace std;
+
+void FillMatrixWithRandomNumbers(int arr1[3][3], int Rows, int Cols) {
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Cols; j++) {
+			arr1[i][j] = MyLib::RandomNumber(1, 10);
+		}
+	}
+}
+
+
+void Print3x3Matrix(int arr[3][3], int Rows, int Cols) {
+
+	for (int i = 0; i < Rows; i++) {
+		for (int j = 0; j < Cols; j++) {
+			//cout << setw(3) << arr[i][j] << "  ";
+			printf(" %0*d   ", 2, arr[i][j]);
+		}
+		cout << endl;
+	}
+
+}
+
+
+void PrintMiddleRowMatrix(int arr[3][3], int Rows, int Cols) {
+
+	for (int i = 1; i <=1 ; i++) {
+		for (int j = 0; j < Cols; j++) {
+			//cout << setw(3) << arr[i][j] << "  ";
+			printf(" %0*d   ", 2, arr[i][j]);
+		}
+		cout << endl;
+	}
+
+}
+
+void PrintMiddleColMatrix(int arr[3][3], int Rows, int Cols) {
+
+	for (int i = 0; i <= Rows-1; i++) {
+		for (int j = 1; j <=1; j++) {
+			//cout << setw(3) << arr[i][j] << "  ";
+			printf(" %0*d   ", 2, arr[i][j]);
+		}
+		
+	}
+
+}
+
+
+int main() {
+
+	int arr1[3][3];
+
+	FillMatrixWithRandomNumbers(arr1, 3, 3);
+	cout << "Matrix1: " << endl;
+	Print3x3Matrix(arr1, 3, 3);
+
+
+	cout << "\nMiddle Row of Matrix1: \n";
+	PrintMiddleRowMatrix(arr1, 3, 3);
+
+	cout << "\nMiddle Columns of Matrix1: \n";
+	PrintMiddleColMatrix(arr1, 3, 3);
+
+}
