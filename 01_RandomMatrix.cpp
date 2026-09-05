a77b16fc2c577ba54810a8e7050545f406523a1e
Tarik <tarik.jaidani@gmail.com>
2026-09-05 17:20:48 +0200

    first commit

--- 01_RandomMatrix.cpp
+++ 01_RandomMatrix.cpp
@@ -32,7 +32,7 @@ int main() {
 	int arr[3][3];
 
 	FillMatrixWithRandomNumbers(arr,3,3);
-	cout << "\n The following is a random matrix:\n";
+	cout << "\n The following is a randome matrix:\n";
 	PrintMatrix(arr, 3, 3);
 		
 }
