703f1c06235e8a38a036a60ea8b77e5e0d05d37f
Tarik <tarik.jaidani@gmail.com>
2026-09-06 20:30:02 +0200

    first commit

--- Source.cpp
+++ Source.cpp
@@ -6,12 +6,41 @@
 
 using namespace std;
 
-void fillRandomMatrix(, ) {
 
+void fillRandomMatrix(int Matrix[3][3], int row, int cols) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < cols; j++) {
+			Matrix[i][j] = MyLib::RandomNumber(1,100);
+		}
+	}
 }
+
+void PrintRandomMatrix(int Matrix[3][3], int row, int cols) {
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < cols; j++) {
+			cout << setw(3) << Matrix[i][j] << "    ";
+		}
+		cout << endl;
+	}
+}
+
+void PrintSum(int Matrix[3][3], int row, int cols) {
+	cout <<
+	for (int i = 0; i < row; i++) {
+		for (int j = 0; j < cols; j++) {
+			cout << setw(3) << Matrix[i][j] << "    ";
+		}
+		cout << endl;
+	}
+}
+
+
 
 int main() {
 
-	fillRandomMatrix(3, 3);
+	int Matrix[3][3];
 
+	fillRandomMatrix(Matrix,3, 3);
+	cout << "The following is a 3x3 random matrix: " << endl;
+	PrintRandomMatrix(Matrix,3, 3);
 }
